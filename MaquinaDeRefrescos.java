2d0d7c545529b3edfde9bf7841c1cbac0f851dc7
repositorioedgar <142647158+repositorioedgar@users.noreply.github.com>
2023-08-24 23:36:29 -0400

    MaquinaDeRefresco1

---
+++ MaquinaDeRefrescos.java
@@ -0,0 +1,103 @@
+public class MaquinaDeRefrescos
+{
+    private int costo;
+    private int cantidad;
+    private int caja;
+
+    public MaquinaDeRefrescos()
+    {
+        this.costo = 0;
+        this.cantidad = 0;
+        this.caja = 0;
+    }
+    
+    public MaquinaDeRefrescos(int costo, int cantidad, int caja)
+    {
+        this.costo = costo;
+        this.cantidad = cantidad;
+        this.caja = caja;        
+    }
+
+    public int getCosto()
+    {
+        return this.costo;
+    }
+    
+    public void setCosto(int costo) 
+    {
+        this.costo = costo;
+    }
+    
+    public int getCantidad() 
+    {
+        return cantidad;
+    }
+      
+    public void setCantidad(int cantidad) 
+    {
+        this.cantidad = cantidad;
+    }
+    
+    public int getCaja() 
+    {
+        return this.caja;
+    }
+    public void setCaja(int caja) 
+    {
+        this.caja = caja;
+    }
+    public void solicitarRefresco(int dinero) 
+    {
+        if(dinero == this.getCosto()) {
+           //entrega del refresco previa verificacion
+           if (this.getCantidad() > 0){
+              // Entrega del refresco
+              this.setCantidad(this.getCantidad() - 1);
+              this.setCaja(this.getCaja() + dinero);
+              System.out.println("________________________________");
+              System.out.println("Recoje tu refresco" );
+              System.out.println("Gracias por tu compra");
+              System.out.println("________________________________");
+           }
+            else
+            {
+                System.out.println("______________________________");
+                System.out.println("Lo siento, ya no hay refrescos");
+                System.out.println("______________________________");
+            }
+         }
+        else
+        {
+           System.out.println("____________________");
+           System.out.println("El costo del refresco es: "+this.getCosto());
+           System.out.println("Puedes intentar con otra moneda");
+           System.out.println("____________________");
+        }
+    }
+    
+    public void actualizarPrecio(int nuevoCosto)
+    {
+        this.setCosto(nuevoCosto);
+        System.out.println("____________________");
+        System.out.println("El costo se actualizo a $ "+this.getCosto());
+        System.out.println("____________________");
+    }
+    
+    public void llenarMaquina(int cantidad)
+    {
+        this.setCantidad(this.getCantidad() + cantidad);
+        System.out.println("____________________");
+        System.out.println("LA maquina tiene "+ this.getCantidad() + "refresco" );
+        System.out.println("____________________");
+    }
+    
+    public void mostrarEstado()
+    {
+        System.out.println("____________________");
+        System.out.println("ESTADO DE LA MAQUINA");
+        System.out.println("Costo del refresco: " + this.getCosto());
+        System.out.println("Cantidad de refrescos: " + this.getCantidad());
+        System.out.println("Monto en caja: " + this.getCaja());
+        System.out.println("____________________");
+    }
+}  
